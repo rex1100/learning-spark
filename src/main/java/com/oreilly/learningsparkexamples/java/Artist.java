e2f596b9a5314d95daca0cf3a61a5a9d3a4173d8
brett.recksiedler@gmail.com <newtonpizzacat2>
2016-12-05 16:56:52 -0600

    messed up made changes on wrong head, had to move changes

---
+++ src/main/java/com/oreilly/learningsparkexamples/java/Artist.java
@@ -0,0 +1,7 @@
+package com.oreilly.learningsparkexamples.java;
+
+/**
+ * Created by brett on 04/12/16.
+ */
+public class Artist {
+}
---
+++ src/main/java/com/oreilly/learningsparkexamples/java/SongInfo.java
@@ -0,0 +1,126 @@
+package com.oreilly.learningsparkexamples.java;
+
+import java.io.Serializable;
+
+/**
+ * Created by brett on 04/12/16.
+ */
+public class SongInfo implements Serializable {
+
+    String trackId;
+    String title;
+    String songId;
+    String release;
+    String artistId;
+    String artistMbid;
+    String artistName;
+    Double duration;
+    String artistFamiliarity;
+    Double artistHotttnesss;
+    int year;
+
+    public SongInfo() {
+    }
+
+    public SongInfo(String trackId, String title, String songId, String release, String artistId, String artistMbid, String artistName, Double duration, String artistFamiliarity, Double artistHotttnesss, int year) {
+        this.trackId = trackId;
+        this.title = title;
+        this.songId = songId;
+        this.release = release;
+        this.artistId = artistId;
+        this.artistMbid = artistMbid;
+        this.artistName = artistName;
+        this.duration = duration;
+        this.artistFamiliarity = artistFamiliarity;
+        this.artistHotttnesss = artistHotttnesss;
+        this.year = year;
+    }
+
+    public String getTrackId() {
+        return trackId;
+    }
+
+    public void setTrackId(String trackId) {
+        this.trackId = trackId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSongId() {
+        return songId;
+    }
+
+    public void setSongId(String songId) {
+        this.songId = songId;
+    }
+
+    public String getRelease() {
+        return release;
+    }
+
+    public void setRelease(String release) {
+        this.release = release;
+    }
+
+    public String getArtistId() {
+        return artistId;
+    }
+
+    public void setArtistId(String artistId) {
+        this.artistId = artistId;
+    }
+
+    public String getArtistMbid() {
+        return artistMbid;
+    }
+
+    public void setArtistMbid(String artistMbid) {
+        this.artistMbid = artistMbid;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public Double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Double duration) {
+        this.duration = duration;
+    }
+
+    public String getArtistFamiliarity() {
+        return artistFamiliarity;
+    }
+
+    public void setArtistFamiliarity(String artistFamiliarity) {
+        this.artistFamiliarity = artistFamiliarity;
+    }
+
+    public Double getArtistHotttnesss() {
+        return artistHotttnesss;
+    }
+
+    public void setArtistHotttnesss(Double artistHotttnesss) {
+        this.artistHotttnesss = artistHotttnesss;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
